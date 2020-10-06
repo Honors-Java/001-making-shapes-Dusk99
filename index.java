7e1056e9cd71788d3bc7db1b943b5fb61f75eb62
Dusk99 <Diddaboy04@gmail.com>
2020-10-06 00:33:12 +0000

    nothing i just didn't actually turn it in

--- index.java
+++ index.java
@@ -13,12 +13,16 @@ void draw() {
  
   //this rectangle is the start of your army tank
   //all of your programming should be after this 👍 
-  rect(200, 200, 100, 50);
+  rect(210, 200, 140, 50);
   
-  
- 
-
-  
+  rect (165, 160, 50, 25)
+  rect (252, 160, 125, 10)
+ellipse (150, 225, 25, 25)
+ellipse (175, 225, 25, 25)
+ellipse (200, 225, 25, 25)
+ellipse (225, 225, 25, 25)
+ellipse (250, 225, 25, 25)
+ellipse (275, 225, 25, 25)
  
 	
 
